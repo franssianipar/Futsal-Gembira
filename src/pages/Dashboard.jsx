6ffe6e43771963ba39bcd861b0fe1f67b38465ee
frans <fransxianiparz@gmail.com>
2023-04-09 10:47:52 +0700

    chart

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,51 @@
 import React from "react";
+import {
+  Chart as ChartJS,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+import ChartDataLabels from "chartjs-plugin-datalabels";
+ChartJS.register(
+  ChartDataLabels,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend
+);
 
 export default function Dashboard() {
+  const data = {
+    labels: [
+      "Agustus 2022",
+      "September 2022",
+      "Oktober 2022",
+      "November 2022",
+      "Desember 2022",
+      "Januari 2022",
+    ],
+    plugins: [ChartDataLabels],
+    datasets: [
+      {
+        data: [25.73, 16.32, 16.32, 20.46, 10.2, 33.99],
+        backgroundColor: ["#FFE600", "#FFE600", "#00D1FF"],
+        datalabels: {
+          color: "White",
+          align: "top",
+          anchor: "end",
+          font: {
+            weight: "bold",
+            size: 20,
+          },
+        },
+      },
+    ],
+  };
+
   return (
     <>
       <div className="bg-[#2F2F2F]">
@@ -19,13 +64,13 @@ export default function Dashboard() {
           </div>
         </div>
       </div>
-      <div className="  bg-[#7E7E7E] pt-12 bg-[#363636]">
-        <div className="mx-20 border-2">
+      <div className=" py-5 bg-[#363636] pt-12 ">
+        <div className="mx-20 border-2 py-10">
           <div className="flex static mx-48 ">
-            <div className=" absolute left-10 bg-[#7E7E7E] bg-[#363636] top-32 text-white text-3xl">
+            <div className=" absolute left-10 bg-[#363636] top-32 text-white text-3xl">
               Dashboard
             </div>
-            <div className=" absolute right-24 bg-[#7E7E7E] bg-[#363636] top-32 text-lg text-white ml-auto">
+            <div className=" absolute right-24 bg-[#363636] top-32 text-lg text-white ml-auto">
               Sabtu,21 Januari 2023
             </div>
           </div>
@@ -47,7 +92,52 @@ export default function Dashboard() {
               </a>
             </div>
           </div>
-          <div className="text-white border-2 ">halo sayang</div>
+          <div className="mt-10 ml-48 w-4/6 border-2 rounded-lg   ">
+            <Bar
+              className=" px-20 py-10 "
+              data={data}
+              options={{
+                scales: {
+                  x: {
+                    stacked: true,
+                    ticks: {
+                      color: ["white"],
+                      beginAtZero: true,
+                      font: { size: 20 },
+                    },
+                    grid: {
+                      display: false,
+                    },
+                    border: {
+                      width: 5,
+                      color: ["white"],
+                      z: 10,
+                    },
+                  },
+                  y: {
+                    beginAtZero: true,
+                    grace: "10%",
+                    stacked: true,
+                    ticks: {
+                      color: ["white"],
+                      display: false,
+                    },
+                    grid: {
+                      display: false,
+                    },
+                    border: {
+                      display: false,
+                    },
+                  },
+                },
+                plugins: {
+                  legend: {
+                    display: false,
+                  },
+                },
+              }}
+            ></Bar>
+          </div>
         </div>
       </div>
     </>
